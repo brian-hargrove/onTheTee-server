89e13515bf8afad4528b72e79682c29dc8c6cf62
Brian <brian.hargrove@outlook.com>
2020-11-30 00:21:58 -0500

    first deploy

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("nowonthetee","postgres","Morgan7576", {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
